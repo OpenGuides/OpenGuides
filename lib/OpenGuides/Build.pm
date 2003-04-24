9973117a5db13756cbe167a391a397c04eb6e425
Kake Pugh <kake@earth.li>
2003-04-24 16:26:38 +0000

    Rejigged in preparation for supporting mysql and sqlite

--- lib/OpenGuides/Build.pm
+++ lib/OpenGuides/Build.pm
@@ -4,21 +4,22 @@ use strict;
 use Module::Build;
 use base 'Module::Build';
 
-eval "use Config::Tiny";
-die "Config::Tiny is required to configure this application.\n" if $@;
-
-eval "use CGI::Wiki::Setup::Pg";
-die "CGI::Wiki is required to set up this application.\n" if $@;
-
-my $config = Config::Tiny->read("wiki.conf");
-my $dbname = $config->{_}->{dbname};
-my $dbuser = $config->{_}->{dbuser};
-my $dbpass = $config->{_}->{dbpass};
-my $dbhost = $config->{_}->{dbhost};
-
 sub ACTION_install {
     my $self = shift;
     $self->SUPER::ACTION_install;
+
+    eval "use Config::Tiny";
+    die "Config::Tiny is required to set up this application.\n" if $@;
+
+    eval "use CGI::Wiki::Setup::Pg";
+    die "CGI::Wiki is required to set up this application.\n" if $@;
+
+    my $config = Config::Tiny->read("wiki.conf");
+    my $dbname = $config->{_}->{dbname};
+    my $dbuser = $config->{_}->{dbuser};
+    my $dbpass = $config->{_}->{dbpass};
+    my $dbhost = $config->{_}->{dbhost};
+
     print "Checking database schema...\n";
     CGI::Wiki::Setup::Pg::setup( $dbname, $dbuser, $dbpass, $dbhost );
 }
