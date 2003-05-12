1afe16d3309f40286ba04101dabcfffcea462033
Kake Pugh <kake@earth.li>
2003-05-12 14:24:27 +0000

    Avoid uninitialised value warning

--- lib/OpenGuides/Template.pm
+++ lib/OpenGuides/Template.pm
@@ -162,7 +162,7 @@ sub extract_tt_vars {
     # rather than just CGI::escapeHTMLing it all in the template.
     my $website = $args{metadata} ? $metadata{website}[0]
                                   : $q->param("website");
-    my $formatted_website_text;
+    my $formatted_website_text = "";
     if ( $website ) {
         $formatted_website_text = $class->format_website_text(
             formatter => $formatter,
