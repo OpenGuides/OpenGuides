d95d57d18b6dd7f208be867fa265158b87eaa9c4
Kake Pugh <kake@earth.li>
2004-06-10 15:53:01 +0000

    Update copyright notice.

--- lib/OpenGuides.pm
+++ lib/OpenGuides.pm
@@ -278,7 +278,7 @@ The OpenGuides Project (openguides-dev@openguides.org)
 
 =head1 COPYRIGHT
 
-     Copyright (C) 2003 The OpenGuides Project.  All Rights Reserved.
+     Copyright (C) 2003-4 The OpenGuides Project.  All Rights Reserved.
 
 The OpenGuides distribution is free software; you can redistribute it
 and/or modify it under the same terms as Perl itself.
