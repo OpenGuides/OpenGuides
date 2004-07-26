ff5d3f1f33f3c67463a125af2a341f2f73465a85
Kake Pugh <kake@earth.li>
2004-07-26 16:39:28 +0000

    Update version, changes and credits ready for release. (couple more changes to come first)

--- lib/OpenGuides.pm
+++ lib/OpenGuides.pm
@@ -12,7 +12,7 @@ use URI::Escape;
 
 use vars qw( $VERSION );
 
-$VERSION = '0.37';
+$VERSION = '0.38';
 
 =head1 NAME
 
@@ -641,9 +641,10 @@ and/or modify it under the same terms as Perl itself.
 
 =head1 CREDITS
 
-Programming by Dominic Hargreaves, Earle Martin, Kake Pugh, Ivor Williams.
-Testing and bug reporting by Cal Henderson, Bob Walker, Kerry Bosworth,
-Simon Cozens, among others.  Much of the Module::Build stuff copied from
+Programming by Dominic Hargreaves, Earle Martin, Kake Pugh, and Ivor
+Williams.  Testing and bug reporting by Billy Abbott, Jody Belka,
+Kerry Bosworth, Simon Cozens, Cal Henderson, Steve Jolly, and Bob
+Walker (among others).  Much of the Module::Build stuff copied from
 the Siesta project L<http://siesta.unixbeard.net/>
 
 =cut
