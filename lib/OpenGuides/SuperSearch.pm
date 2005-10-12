aad5331eff0cd8150cb9e02ebec5b249aaf5167b
earle <hex@downlode.org>
2005-10-12 01:49:28 +0000

    always pass latitude and longitude to the template if criteria for distance search are defined

--- lib/OpenGuides/SuperSearch.pm
+++ lib/OpenGuides/SuperSearch.pm
@@ -155,6 +155,8 @@ sub run {
         $doing_search = 1;
         # Make sure to pass the criteria to the template.
         $tt_vars{dist} = $self->{distance_in_metres};
+        $tt_vars{latitude} = $self->{latitude};
+        $tt_vars{longitude} = $self->{longitude};
         if ( $self->config->geo_handler eq 1 ) {
             $tt_vars{coord_field_1_value} = $self->{os_x};
             $tt_vars{coord_field_2_value} = $self->{os_y};
