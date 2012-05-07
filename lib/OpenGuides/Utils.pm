a59ea5a5576dcb14a8ca5049f12bd63516f576be
bob walker <bob@randomness.org.uk>
2012-05-07 09:59:27 +0000

    bump version numbers and update copyright on changed modules.

--- lib/OpenGuides/Utils.pm
+++ lib/OpenGuides/Utils.pm
@@ -2,7 +2,7 @@ package OpenGuides::Utils;
 
 use strict;
 use vars qw( $VERSION );
-$VERSION = '0.12';
+$VERSION = '0.13';
 
 use Carp qw( croak );
 use Wiki::Toolkit;
@@ -545,7 +545,7 @@ The OpenGuides Project (openguides-dev@lists.openguides.org)
 
 =head1 COPYRIGHT
 
-     Copyright (C) 2003-2008 The OpenGuides Project.  All Rights Reserved.
+     Copyright (C) 2003-2012 The OpenGuides Project.  All Rights Reserved.
 
 This module is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
