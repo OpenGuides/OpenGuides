46e8143d42f9aeddbdb7cbf2b2e1e6effda219e4
Kake Pugh <kake@earth.li>
2012-07-22 13:18:29 +0000

    Made the popups on the maps actually join up with the pointers.

--- static/map-leaflet.js
+++ static/map-leaflet.js
@@ -6,7 +6,7 @@ var gicon = L.Icon.extend( {
     shadowUrl: null,
     iconSize: new L.Point( 32, 32 ),
     iconAnchor: new L.Point( 15, 32 ),
-    popupAnchor: new L.Point( 0, -40 )
+    popupAnchor: new L.Point( 0, -30 )
 } );
 
 $(
